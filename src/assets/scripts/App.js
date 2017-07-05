9377ced211366d35dab3b5d7ab6500e28d5fe901
PEH2 <howley.phil@gmail.com>
2017-07-05 00:12:26 -0700

    complete modal overlay

--- src/assets/scripts/App.js
+++ src/assets/scripts/App.js
@@ -4,12 +4,14 @@ import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
 import HighlightSection from './modules/HighlightSection';
 import SmoothScrolling from './modules/SmoothScrolling';
+import Modal from './modules/Modal';
 
 /* eslint-disable no-unused-vars */
 const mobileMenu = new MobileMenu();
 const stickyHeader = new StickyHeader();
 const highlightSection = new HighlightSection();
 const smoothScrolling = new SmoothScrolling();
+const modal = new Modal();
 /* eslint-disable no-new */
 new RevealOnScroll($('.project-item'), '85%');
 new RevealOnScroll($('.tool'), '85%');
